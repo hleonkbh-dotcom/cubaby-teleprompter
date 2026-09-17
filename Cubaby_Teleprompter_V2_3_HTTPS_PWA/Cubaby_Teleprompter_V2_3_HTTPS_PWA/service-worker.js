a51740d5ab2d2c0f4fad2f6b03b2a00bd9adfd6f
hleonkbh-dotcom <hleon.kbh@gmail.com>
2026-09-17 13:12:22 -0600

    Add files via upload

---
+++ Cubaby_Teleprompter_V2_3_HTTPS_PWA/Cubaby_Teleprompter_V2_3_HTTPS_PWA/service-worker.js
@@ -0,0 +1,25 @@
+const CACHE = "cubaby-v2-3-1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest",
+  "./assets/icons/icon-192.png",
+  "./assets/icons/icon-512.png",
+  "./assets/icons/apple-touch-icon.png"
+];
+self.addEventListener("install", event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
+  self.skipWaiting();
+});
+self.addEventListener("activate", event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))));
+  self.clients.claim();
+});
+self.addEventListener("fetch", event => {
+  if (event.request.method !== "GET") return;
+  event.respondWith(caches.match(event.request).then(hit => hit || fetch(event.request).then(response => {
+    const copy = response.clone();
+    caches.open(CACHE).then(cache => cache.put(event.request, copy));
+    return response;
+  }).catch(() => caches.match("./index.html"))));
+});
